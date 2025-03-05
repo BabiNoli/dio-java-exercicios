e9cd518bc5ddc9c1df31486d37d7a575f4c44ba2
BarbaraNoli <barbara.it.noli@gmail.com>
2025-03-05 12:29:26 +0100

    adicionei scanner e Exception

--- exercicios_aula/area_quadrado.java
+++ exercicios_aula/area_quadrado.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class area_quadrado {
     public static void main(String[] args) {
-        double lado = 5;
-        double area = lado * lado;
-        System.out.println("A área do quadrado é: " + area);
-    }
+        try (var scanner = new Scanner(System.in)) {
+            System.out.println("Digite o lado do quadrado: ");
+            double lado = scanner.nextDouble();
+            double area = lado * lado;
+            System.out.println("A área do quadrado é: " + area);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao calcular a área do quadrado", e);
+        }
+    } 
 }
 
