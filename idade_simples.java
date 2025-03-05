84830d9ea0be6d31974d73342d40df6f4bbaf95a
Barbara Noli <2003471@estudante.uab.pt>
2025-03-05 11:14:17 +0100

    adicionando primeiro arquivo

---
+++ idade_simples.java
@@ -0,0 +1,15 @@
+public class idade_simples {
+    public static void main(String[] args) {
+        var baseYear = OffsetDateTime.now().getYear();
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe o seu nome:");
+        var name = scanner.next();
+
+        System.out.println("Digite o ano de nascimento: ");
+        var birthYear = scanner.nextInt();
+
+        var age = baseYear - birthYear;
+        System.out.printf("Olá %s, você tem %d anos.\n", name, age);
+        
+    }
+}
