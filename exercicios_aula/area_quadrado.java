7364b4bb6f8b9a8bc684063cd328a07b084b4d53
BarbaraNoli <barbara.it.noli@gmail.com>
2025-03-05 12:09:22 +0100

    add arquivo area quadrado

---
+++ exercicios_aula/area_quadrado.java
@@ -0,0 +1,8 @@
+public class area_quadrado {
+    public static void main(String[] args) {
+        double lado = 5;
+        double area = lado * lado;
+        System.out.println("A área do quadrado é: " + area);
+    }
+}
+
