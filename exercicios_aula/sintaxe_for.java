6755bff4aeda32d9bb25bd67b9e31bbbc06dc02b
BarbaraNoli <barbara.it.noli@gmail.com>
2025-03-06 14:46:51 +0100

    add for

--- exercicios_aula/sintaxe_for.java
+++ exercicios_aula/sintaxe_for.java
@@ -1,19 +1,57 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class sintaxe_for {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
-        for (;;){
-            System.out.println("Digite um nome: ");
-            var name = scanner.next();
+        try {
+            List<String> names = new ArrayList<>();
 
-            if(name.equalsIgnoreCase("exit")) break;
+            //dessa forma o for entra num loop inifinito se nao houver um comando de paragem no corpo do for
+            for (;;){
+                System.out.println("Digite um nome: ");
+                String name = scanner.nextLine();
 
-            System.out.println("");
+                boolean isduplicate = false;
+                for(String existingName : names){
+                    if (existingName.equalsIgnoreCase(name)){
+                        isduplicate = true;
+                        break; //sai do for
+                    }
+                }
+
+                if (isduplicate){
+                    System.out.println("--------------------------------");
+                    System.out.println("Nome repetido! Interrompendo...");
+                    System.out.println("--------------------------------");
+                    break; 
+                } else {
+                    names.add(name);
+                }
+            }
+            
+            System.out.println("Nomes digitados:");
+            for (var i = 0; i < names.size(); i++) {
+                if (i == names.size() - 1) {
+                    System.out.print(names.get(i) + ".");
+                } else {
+                    System.out.print(names.get(i) + ", ");
+            }
         }
-
+        } finally {
+            scanner.close();
+        }
+        
     }
 
-
+    /*também pode usar o for da seguinte forma:
+    var i = 2;  //inicializacao da variavel de controle fora do for
+    for(;i < 100; i+=2){  //vai de 2 em 2
+        if(i % 2 == 0){
+            continue;    //nesse caso ele iria ignorar os pares porque o continue faria ele seguir para a iteracao seguinte
+        }
+        ...
+    }*/
     
 }
